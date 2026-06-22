dcb6c50c4c000ec4b4e3880f6249299b8b950d24
felipe acuna <acunafelipe272@gmail.com>
2026-06-22 03:55:20 +0000

    feat(auth): se sube la versión con documentación y test

--- repartidor/src/main/java/com/tiendacarta/repartidor/Controller/RepartidorController.java
+++ repartidor/src/main/java/com/tiendacarta/repartidor/Controller/RepartidorController.java
@@ -17,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tiendacarta.repartidor.DTO.RepartidorCrateDTO;
 import com.tiendacarta.repartidor.DTO.RepartidorDTO;
 import com.tiendacarta.repartidor.Service.RepartidorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import jakarta.validation.Valid;
 
+@Tag(name="repartidores",description = "Operaciones de gestion de repartidores")
 @RestController
 @RequestMapping("api/v2/repartidores")
 public class RepartidorController {
@@ -27,32 +33,69 @@ public class RepartidorController {
     @Autowired
     private  RepartidorService repartidorService;
 
+    @Operation(
+        summary = "Listar todas los repartidores",
+        description = "Retorna la lista completa de repartidores registradas en el sistema."
+    )
+    @ApiResponse(responseCode = "200", description = "Lista obtenida exitosamente")
     @GetMapping
     public ResponseEntity<List<RepartidorDTO>> getAll(){
         return ResponseEntity.ok(repartidorService.findall());
     }
+
+    @Operation(summary = "Buscar repartidores por ID")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "repartidores encontrada"),
+        @ApiResponse(responseCode = "404", description = "repartidores no encontrada")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<RepartidorDTO> getById(@PathVariable Long id){
+    public ResponseEntity<RepartidorDTO> getById(@Parameter(description = "Id unico de la Usuario",required = true)@PathVariable Long id){
         return ResponseEntity.ok(repartidorService.findById(id));
     }
 
+
+    @Operation(summary = "Registrar nueva repartidores")
+    @ApiResponse(responseCode = "201", description = "repartidores creada exitosamente")
+    @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos")
     @PostMapping
-    public ResponseEntity<RepartidorDTO> crear(@Valid @RequestBody RepartidorCrateDTO dto) {
+    public ResponseEntity<RepartidorDTO> crear(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                description = "Datos del nuevo repartidores"
+            )
+            @Valid @RequestBody RepartidorCrateDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(repartidorService.crear(dto));
     }
-
+    @Operation(summary = "Eliminar repartidores")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Eliminación exitosa"),
+        @ApiResponse(responseCode = "404", description = "repartidores no encontrada")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+    public ResponseEntity<Void> eliminar(
+            @Parameter(description = "ID del repartidores a eliminar", required = true)
+            @PathVariable Long id) {
         repartidorService.eliminar(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<RepartidorDTO> actualizar(@PathVariable Long id, @RequestBody RepartidorCrateDTO dto){
-        RepartidorDTO reparidorActualizado = repartidorService.actualizar(id, dto);
-        return new ResponseEntity<>(reparidorActualizado,HttpStatus.OK);
 
+    @Operation(summary = "Actualizar repartidores existente")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Actualización exitosa"),
+        @ApiResponse(responseCode = "404", description = "repartidores no encontrada"),
+        @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos")
+    })
+    @PutMapping("/{id}")
+    public ResponseEntity<RepartidorDTO> actualizar(
+            @Parameter(description = "ID del repartidores a actualizar", required = true)
+            @PathVariable Long id,
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                description = "Nuevos datos del repartidores"
+            )
+            @Valid @RequestBody RepartidorCrateDTO dto) {
+        return ResponseEntity.ok(repartidorService.actualizar(id, dto));
     }
+
 
     
 }
--- repartidor/src/main/java/com/tiendacarta/repartidor/DTO/RepartidorCrateDTO.java
+++ repartidor/src/main/java/com/tiendacarta/repartidor/DTO/RepartidorCrateDTO.java
@@ -2,25 +2,32 @@ package com.tiendacarta.repartidor.DTO;
 
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "Datos necesarios para crear o actualizar un Repartidor")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RepartidorCrateDTO {
-
+    
+    @Schema(description = "id de usuario", example = "1")
     @NotNull(message = "ingresa el id del usuario")
     private Long usuarioId;
-    @NotNull(message = "ingrese el id del auto")
+    /*dejarlo sin validacion */
+    @Schema(description = "id de vehiculo", example = "1")
     private Long autoId;
+    @Schema(description = "cantidad de envio", example = "1")
     @NotNull(message = "ingresa la cantidad de envio")
     private Integer cantidadEnvio;
+    @Schema(description = "Historial de envio", example = "rancagua,dexnimension,delux clasis carta,completado,35.0000")
     @NotNull(message = "ingrese el historial de envio")
     private String historialEnvio;
+    @Schema(description = "tienda de origen", example = "nex dimencion")
     @NotNull(message = "ingrese la tienda de origen")
     private String tiendaOrigen;
     
